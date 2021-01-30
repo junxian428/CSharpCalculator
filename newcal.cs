5c35de49fc3a6c3de6a4111644a1ad0fe5412f43
junxian428 <58724748+junxian428@users.noreply.github.com>
2021-01-30 21:02:44 +0000

    Add files via upload

---
+++ newcal.cs
@@ -0,0 +1,47 @@
+using System;
+
+namespace Ho
+{
+ class newcal
+ {
+    static void Main(string[] args)
+    {
+     Console.Write("Enter a number: ");
+     double num1 = Convert.ToDouble(Console.ReadLine());
+     
+     Console.Write("Enter operator: ");
+     string op = Console.ReadLine();
+
+     Console.Write("Enter a number: ");
+     double num2 = Convert.ToDouble(Console.ReadLine());
+     
+     if(op == "+")
+     {
+       Console.WriteLine(num1 + num2);
+
+     } else if (op == "-")
+     {
+
+           Console.WriteLine(num1 - num2);
+     } else if (op == "*")
+     {
+       Console.WriteLine(num1 * num2);
+
+     } else if (op == "/")
+     {
+
+        Console.WriteLine(num1 / num2);
+     } else
+
+     {
+
+         Console.WriteLine("Invalid Operator");
+     }
+
+     Console.ReadLine();
+     
+    }
+
+ }
+
+}
